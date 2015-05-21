3d727077e62f8c9d89f7c6a4e1b255d629bfda96
Cypheruim <ryanhuang1234@hotmail.com>
2015-05-21 11:47:42 -0400

    Create Mobs.java

---
+++ Mobs.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Mobs here.
+ * 
+ * @author Ryan Huang
+ * @version May 2015
+ */
+public class Mobs extends Actor
+{
+    protected double baseHp;
+    protected double baseAtt;
+    protected double baseDef;
+    protected double baseDex;
+    protected double baseXp;
+    protected double baseMov;
+}
