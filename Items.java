8a041adb4af7444649baccadbd4f3f19dd55f9ed
jackyyang09 <jackyyang267@gmail.com>
2015-05-27 11:10:26 -0400

    Update and rename Item.java to Items.java

---
+++ Items.java
@@ -0,0 +1,52 @@
+import greenfoot.*;
+
+/**
+ * Universal Item class
+ * 
+ * @author Jacky Yang
+ * @version (a version number or a date)
+ */
+public class Items extends Actor
+{
+    private int id;
+    private String name;
+    private String fileName
+    private int equipType;
+    private int atkBuff;
+    private int defBuff;
+    public Items(int num)
+    {
+        if (num == 1)
+        {
+            id = num;
+            name = "Beam Blade";
+            equipType = 1;
+            setImage("beamblade1.png");
+            fileName = "beamblade3.png");
+            atkBuff = 2;
+            defBuff = 0;
+        }
+    }
+    
+    /**
+     * Create custom weapon, debugging purposes
+     */
+    public Items(String name, int equipType, String image, int atkVar, int defVar)
+    {
+        this.name = name;
+        this.equipType = equipType;
+        setImage(image);
+        atkBuff = atkVar;
+        defBuff = defVar;
+    }
+    
+    public String getImageFile()
+    {
+        return fileName;
+    }
+    
+    public int getEquipType()
+    {
+        return equipType;
+    }
+}
