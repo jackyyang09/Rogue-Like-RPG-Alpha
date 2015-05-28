e11bf7d18c11f232296b70962c13584ae81bb3a1
sean21x <sean21x@users.noreply.github.com>
2015-05-27 22:17:04 -0400

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.Random;
 /**
  * Write a description of class Dirt here.
  * 
@@ -10,16 +10,23 @@ public class Tile extends Actor
 {
     int mapX;
     int mapY;
+    int randFloor;
     public Tile(int getMapX, int getMapY, int tileCode)
     {
+        Random rnd = new Random();
         if(tileCode == 1){
+            randFloor = rnd.nextInt(2) + 1;
             mapX = getMapX;
             mapY = getMapY;
-            setImage("dirt.jpg");
+            if(randFloor == 1){
+                setImage("floorTile.png");
+            } else {
+                setImage("floorTile2.png");
+            }
         } else if(tileCode == 2){
             mapX = getMapX;
             mapY = getMapY;
-            setImage("stone.png");
+            setImage("dirt.jpg");
         }
     }
 }
