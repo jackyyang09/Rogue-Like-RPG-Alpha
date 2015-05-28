a673268c9509c03830e660cc40d51e93bd74137a
Cypheruim <ryanhuang1234@hotmail.com>
2015-05-28 19:33:37 -0400

    Update Generate.java

--- Generate.java
+++ Generate.java
@@ -13,29 +13,30 @@ public class Generate
     public void generateMap(int lvl){
 
     }
-    
-    public void generateBorder(){
+
+    public String[][][] generateBorder(){
         // 4x3, 4x52, 53x3, 53x52
-        for(int i = 3; i < 52; i++){
+        for(int i = 4; i < 52; i++){
             array[4][i][0] = "border";
             array[53][i][0] = "border";
         }
-        for(int j = 4; j < 53; j++){
+        for(int j = 4; j < 54; j++){
             array[j][4][0] = "border";
             array[j][52][0] = "border";
         }
+        return array;
     }
 
     public void generateStartRoom(){
 
     }
-    
+
     public void generateEndRoom(){
-        
+
     }
-    
+
     public void generateRoom(){
-        
+
     }
 
     public String[][][] returnMap(){
