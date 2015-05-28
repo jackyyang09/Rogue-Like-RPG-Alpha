3e331a3ae3a61d0e0d78be5fcfae4f1eac72ec89
jackyyang09 <jackyyang267@gmail.com>
2015-05-28 12:27:38 -0400

    Update HUD.java

--- HUD.java
+++ HUD.java
@@ -14,23 +14,71 @@ import java.awt.Font;
  */
 public class HUD extends Actor
 {
-    ValueBox box;
+    private boolean begin;
+    private boolean open = false;
+    private ValueBox box;
+    private Button bp;
+    private Inventory inv;
     public HUD()
     {
+        begin = false;
         box = new ValueBox();
+        bp = new Button();
     }
-    
+
+    public void begin()
+    {
+        bp.setImage("Sack.png");
+        getWorld().addObject(bp, getX() + 200, getY());
+        getWorld().addObject(box, 413, 631);
+        begin = true;
+    }
+
     /**
      * Act - do whatever the HUD wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
+        if (begin == false){begin();}
+        mouseDetect();
         update();
     }   
 
+    /**
+     * Detects mouse presses
+     */
+    private void mouseDetect()
+    {
+        if (Greenfoot.mousePressed(bp) == true)
+        {
+            bp.setImage("SackClick.png");
+        }
+        if (Greenfoot.mouseClicked(bp) == true)
+        {
+            if (open == true)
+            { 
+                inv.close();
+                bp.setImage("Sack.png");
+                open = false;
+            }
+            else
+            {
+                inv = new Inventory();
+                getWorld().addObject(inv, getX() + 232, getY() - 385);
+                bp.setImage("SackOpen.png");
+                open = true;
+            }
+        }
+        else if (Greenfoot.mouseClicked(null) == true)
+        {
+            if (open == false){bp.setImage("Sack.png");}
+            else{bp.setImage("SackOpen.png");}
+        }
+    }
+
     private void update()
     {
-        getWorld().addObject(box, 413, 631);
+
     }
 }
