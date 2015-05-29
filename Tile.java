1577f0214c4b25b01d68cf6428701c3a6c4abc0e
Cypheruim <ryanhuang1234@hotmail.com>
2015-05-29 11:59:40 -0400

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -3,7 +3,7 @@ import java.util.Random;
 /**
  * Write a description of class Dirt here.
  * 
- * @author (your name) 
+ * @author Ryan Huang
  * @version (a version number or a date)
  */
 public class Tile extends Actor
@@ -13,20 +13,14 @@ public class Tile extends Actor
     int randFloor;
     public Tile(int getMapX, int getMapY, int tileCode)
     {
-        Random rnd = new Random();
         if(tileCode == 1){
-            randFloor = rnd.nextInt(2) + 1;
-            mapX = getMapX;
-            mapY = getMapY;
-            if(randFloor == 1){
-                setImage("floorTile.png");
-            } else {
-                setImage("floorTile2.png");
-            }
-        } else if(tileCode == 2){
-            mapX = getMapX;
-            mapY = getMapY;
-            //setImage("dirt.jpg");
+            setImage("floorTile.png");
+        }
+        if(tileCode == 2){
+            setImage("floorTile2.png");
+        }
+        if(tileCode == 3){
+            setImage("wall.png");
         }
     }
 }
