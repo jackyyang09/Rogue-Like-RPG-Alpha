deebe6e33cef4ae0c96c26a179973f58371ce4f4
sean21x <sean21x@users.noreply.github.com>
2015-05-28 20:01:10 -0400

    Create Control.java

---
+++ Control.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Control here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Control extends Actor
+{
+    
+    /**
+     * Act - do whatever the Control wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        //((ScrollingMap)getWorld).centerOnPlayer();
+    }    
+    
+}
