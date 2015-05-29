9a949c0620d35b6eade7efb95600f4e9f9d4f45e
jackyyang09 <jackyyang267@gmail.com>
2015-05-29 12:03:44 -0400

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -11,6 +11,7 @@ public class Inventory extends Actor
 {
     private Items[] items;
     private Items[] equips;
+    private Items[] slots;
     private boolean update;
     public Inventory()
     {
@@ -32,6 +33,7 @@ public class Inventory extends Actor
     public void update()
     {
         clear();
+        slots = new Items[11];
         List<Player> Playerlist = getWorld().getObjects(Player.class);
         for(Player P : Playerlist)
         {
@@ -39,36 +41,86 @@ public class Inventory extends Actor
             equips = P.getEquips();
             for (int i = 0; i < Array.getLength(items); i++)
             {
-                if (items[i] != null)
-                {            
-                    if (i == 0){getWorld().addObject(new Items(items[i].getItemID(), true), 582, 111);}
-                    if (i == 1){getWorld().addObject(new Items(items[i].getItemID(), true), 708, 111);}
-                    if (i == 2){getWorld().addObject(new Items(items[i].getItemID(), true), 830, 111);}
-                    if (i == 3){getWorld().addObject(new Items(items[i].getItemID(), true), 582, 237);}
-                }else if (items[i] == null)
-                {
-                    if (i == 0){getWorld().addObject(new Items(), 582, 111);}
-                    if (i == 1){getWorld().addObject(new Items(), 708, 111);}
-                    if (i == 2){getWorld().addObject(new Items(), 830, 111);}
-                    if (i == 3){getWorld().addObject(new Items(), 582, 237);}
-                    if (i == 4){getWorld().addObject(new Items(), 582, 237);}
-                    if (i == 5){getWorld().addObject(new Items(), 582, 237);}
-                }
+                Items newItem = null;
+                if (items[i] != null){newItem = new Items(items[i].getItemID(), true);}
+                else if (items[i] == null){newItem = new Items();}
+                if (i == 0){getWorld().addObject(newItem, 582, 115);}
+                if (i == 1){getWorld().addObject(newItem, 705, 115);}
+                if (i == 2){getWorld().addObject(newItem, 828, 115);}
+                if (i == 3){getWorld().addObject(newItem, 582, 239);}
+                if (i == 4){getWorld().addObject(newItem, 705, 239);}
+                if (i == 5){getWorld().addObject(newItem, 828, 239);}
+                if (i == 6){getWorld().addObject(newItem, 582, 363);}
+                if (i == 7){getWorld().addObject(newItem, 705, 363);}
+                if (i == 8){getWorld().addObject(newItem, 828, 363);}
+                slots[i] = newItem;
             }
             for (int i = 0; i < Array.getLength(equips); i++)
             {
-                if (equips[i] != null)
-                {
-                    if (i == 0)
-                    {
-                        getWorld().addObject(new Items(equips[i].getItemID(), true), 580, 500);
-                    }
-                }
+                Items newItem = null;
+                if (equips[i] != null){newItem = new Items(equips[i].getItemID(), true);}
+                if (equips[i] == null){newItem = new Items();}
+                if (i == 0){getWorld().addObject(newItem, 582, 503);}
+                if (i == 1){getWorld().addObject(newItem, 705, 503);}
+                slots[i + 9] = newItem;
             }
         }
         update = true;
     }
-    
+
+    /**
+     * Switches the sprites within the inventory screen and sends a command to Player to switch items in their inventory
+     * 
+     * @param item1 The item itself
+     * @param item2 The item it's touching
+     * @return boolean Only used by Items
+     */
+    public boolean switchSlot(Items item1, Items item2)
+    {
+        int slot1 = 0;
+        int slot2 = 0;
+        for (int i = 0; i < Array.getLength(slots); i++)
+        {
+            if (slots[i] == item1)
+            {
+                slot1 = i;
+                i = Array.getLength(slots);
+            }
+        }
+        for (int i = 0; i < Array.getLength(slots); i++)
+        {
+            if (slots[i] == item2)
+            {
+                slot2 = i;
+                i = Array.getLength(slots);
+            }
+        }
+        boolean pass = false;
+        //Check if you're trying to put a non-weapon into a weapon slot or a non-armor into an armor slot
+        if ((slot2 == 9 && item1.getEquipType() == 1) || (slot2 == 10 && item1.getEquipType() == 2)){pass = true;} // Items > Equips
+        else if ((slot1 == 9 && item2.getEquipType() == 1) || (slot1 == 10 && item2.getEquipType() == 2)){pass = true;} // Equips > Items
+        else if ((slot1 == 9 && item2.getItemID() == 0) || (slot1 == 10 && item2.getItemID() == 0)){pass = true;} // Equips > Empty Items
+        else if (slot1 < 9 && slot2 < 9){pass = true;}
+        else{return true;}
+        if (pass)
+        {
+            List<Player> Playerlist = getWorld().getObjects(Player.class);
+            for(Player P : Playerlist)
+            {
+                if (slot1 < slot2)
+                {
+                    int hold;
+                    hold = slot1;
+                    slot1 = slot2;
+                    slot2 = hold;
+                }
+                P.switchSlots(slot1, slot2);
+            }
+            update = false;
+        }
+        return false;
+    }
+
     public void clear()
     {
         List<Items> ItemList = getIntersectingObjects(Items.class);
