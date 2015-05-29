fe4eb1f694a583d55409953aa40f4e0c6affa9aa
krisleung98 <krisleung98@gmail.com>
2015-05-29 12:11:52 -0400

    Update Generate.java

--- Generate.java
+++ Generate.java
@@ -3,18 +3,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Generate here.
  * 
- * @author (your name) 
+ * @author Kris Leung
  * @version (a version number or a date)
  */
 public class Generate
 {
     String array[][][] = new String[58][56][3];
-
-    public void generateMap(int lvl){
-
+    private int width, height, xPlace, yPlace;
+    public String[][][] generateMap(){
+        generateBorder();
+        generateStartRoom();
+        return array;
     }
 
-    public String[][][] generateBorder(){
+    public void generateBorder(){
         // 4x3, 4x52, 53x3, 53x52
         for(int i = 4; i < 52; i++){
             array[4][i][0] = "border";
@@ -24,11 +26,30 @@ public class Generate
             array[j][4][0] = "border";
             array[j][52][0] = "border";
         }
-        return array;
     }
 
     public void generateStartRoom(){
-
+        width = getRandWidth();
+        height = getRandHeight();
+        xPlace = getRandX(width);
+        yPlace = getRandY(height);
+        System.out.println(width);
+        System.out.println(height);
+        System.out.println(xPlace);
+        System.out.println(yPlace);
+        for(int i = xPlace; i < xPlace + width + 1; i++){
+            array[i][yPlace][0] = "wall";
+            array[i][yPlace+height+1][0] = "wall";
+        }
+        for(int j = yPlace; j < yPlace + height + 1; j++){
+            array[xPlace][j][0] = "wall";
+            array[xPlace+width][j][0] = "wall";
+        }
+        for(int k = xPlace + 1; k < xPlace + width; k++){
+            for(int l = yPlace + 1; l < yPlace + height+1; l++){
+                array[k][l][0] = "floorTile";
+            }
+        }
     }
 
     public void generateEndRoom(){
@@ -42,4 +63,18 @@ public class Generate
     public String[][][] returnMap(){
         return array;
     }
+    
+    public int getRandWidth(){
+        return Greenfoot.getRandomNumber(7) +3;
+    }
+    public int getRandHeight(){
+        return Greenfoot.getRandomNumber(7) +3;
+    }
+    public int getRandX(int w){
+        return Greenfoot.getRandomNumber(48-w)+5;
+    }
+    public int getRandY(int h){
+        return Greenfoot.getRandomNumber(47-h) +5;
+    }
+    
 }
