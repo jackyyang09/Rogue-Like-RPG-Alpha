8f9f4599aae02ccde41ea73940647e7c0d794ef5
Cypheruim <ryanhuang1234@hotmail.com>
2015-05-28 20:00:02 -0400

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -30,31 +30,21 @@ public class Enemy extends Mobs
         for(int i = 0; i < 58; i++){
             for(int j = 0; j < 56; j++){
                 if(grid[i][j][1] != null){
-                    int playerX = i;//((Player)grid[i][j][1]).getMapX();
-                    int playerY = j;//((Player)grid[i][j][1]).getMapY();
+                    playerX = i;//((Player)grid[i][j][1]).getMapX();
+                    playerY = j;//((Player)grid[i][j][1]).getMapY();
+
                     //convertToTilePlayer();
                 }
             }
         }
         //int playerX = grid[0][0][2].getMapX();
         //int playerY = grid[0][0][2].getMapY();
-
-        moveTo = bfs.BFSPathFinding(mapX, mapY, 3, 4);
+        moveTo = bfs.BFSPathFinding(mapX, mapY, playerX, playerY);
         bfs.reset();
         middle = moveTo.indexOf('x');
         mapX = Integer.parseInt(moveTo.substring(0,middle));
         mapY = Integer.parseInt(moveTo.substring(middle + 1));
         convertToPixel();
         ((ScrollingMap)getWorld()).update();
-    }
-
-    public void convertToTilePlayer(){
-        playerX = (playerX - 43) / 86;
-        playerY = (playerY - 43) / 86;
-    }
-
-    public void convertToPixelPLayer(){
-        mapX = (mapX * 86) + 43;
-        mapY = (mapY * 86) + 43;
     }
 }
