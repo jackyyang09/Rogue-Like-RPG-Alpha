d7cbd49f6c0b5a012c056510e0c14f9aa2d053ba
sean21x <sean21x@users.noreply.github.com>
2015-05-30 22:28:06 -0400

    Update Control.java

--- Control.java
+++ Control.java
@@ -8,14 +8,24 @@ import greenfoot.*;
  */
 public class Control extends Actor
 {
-    
     /**
-     * Act - do whatever the Control wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Manual testing controls
      */
     public void act() 
     {
-        //((ScrollingMap)getWorld).centerOnPlayer();
-    }    
-    
+        if(Greenfoot.isKeyDown("s")){
+            ((ScrollingMap)getWorld()).movePlayer(1);
+        }
+        if(Greenfoot.isKeyDown("w")){
+            ((ScrollingMap)getWorld()).movePlayer(2);
+        }
+        if(Greenfoot.isKeyDown("d")){
+            ((ScrollingMap)getWorld()).movePlayer(3);
+        }
+        if(Greenfoot.isKeyDown("a")){
+            ((ScrollingMap)getWorld()).movePlayer(4);
+        }
+
+        ((ScrollingMap)getWorld()).update();
+    }
 }
