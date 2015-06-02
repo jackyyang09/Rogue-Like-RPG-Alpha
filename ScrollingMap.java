311a9a8eccff317526785cdcbad12657709bf58a
jackyyang09 <jackyyang267@gmail.com>
2015-06-02 17:44:14 -0400

    Update ScrollingMap.java

--- ScrollingMap.java
+++ ScrollingMap.java
@@ -34,8 +34,7 @@ public class ScrollingMap extends World
     public ScrollingMap()
     {    
         super(946, 774, 1, false);
-        setPaintOrder(Button.class, ItemInventory.class, ValueBox.class, HUD.class, Inventory.class, Player.class, Items.class, Mobs.class, Tile.class);
-        createMap(generate.generateMap());
+        setPaintOrder(InfoTab.class, Button.class, ItemInventory.class, ValueBox.class, HUD.class, Inventory.class, Player.class, Items.class, Mobs.class, Tile.class);
         spawnPlayer();
         centerOnPlayer();
         update();
