1fb47223122b23e0240aed6cf14ac33d11581466
jackyyang09 <jackyyang267@gmail.com>
2015-06-04 23:53:35 -0400

    Update InfoTab.java

--- InfoTab.java
+++ InfoTab.java
@@ -1,6 +1,5 @@
 import greenfoot.*;
 import java.awt.Font;
-import java.lang.reflect.Array;
 /**
  * Displays a window showing detailed information when hovering over an item
  * 
@@ -12,14 +11,12 @@ public class InfoTab extends Actor
     ItemInventory items;
     private boolean begin = false;
     private Font fontName, fontDesc, fontNums;
-    private String[] description;
     public InfoTab(ItemInventory iteminventory)
     {
         items = iteminventory;
         fontName = new Font("OCR A Extended", 2, 20);
         fontDesc = new Font("OCR A Extended", 1, 13);
         fontNums = new Font("OCR A Extended", 3, 20);
-        setImage("Info.png");
     }
 
     /**
@@ -28,26 +25,26 @@ public class InfoTab extends Actor
      */
     public void act() 
     {
-        if (begin == false)
-        {
-            begin();
-        }
+        if (begin == false){begin();} //Run only once
     }    
 
+    /**
+     * Method used to obtain data the item database and display it onto the original image
+     */
     public void begin()
     {
         GreenfootImage image = new GreenfootImage(getImage());
         image.setFont(fontName);
-        image.drawImage(items.getImage(), 23, 28);
-        Items item = new Items(items.getItemID());
-        image.drawString(item.getName(), 213, 56);
+        image.drawImage(items.getImage(), 23, 28); //Create item image in top left corner
+        Items item = new Items(items.getItemID()); //Obtains references from the item database
+        image.drawString(item.getName(), 213, 56); //Generate item's name
         image.setFont(fontDesc);
-        image.drawString(item.getDescription(), 155, 85);
+        image.drawString(item.getDescription(), 155, 85); //Generate item's description
         image.setFont(fontNums);
-        image.drawString(Integer.toString(item.getAtkBuff()), 65, 167);
-        image.drawString(Integer.toString(item.getDefBuff()), 120, 167);
-        image.drawString(Integer.toString(item.getLukBuff()), 65, 192);
-        image.drawString(Integer.toString(item.getDexBuff()), 120, 192);
+        image.drawString(Integer.toString(item.getAtkBuff()), 65, 167); //Generate attack stat
+        image.drawString(Integer.toString(item.getDefBuff()), 120, 167); //Generate defense stat
+        image.drawString(Integer.toString(item.getLukBuff()), 65, 192); //Generate luck stat
+        image.drawString(Integer.toString(item.getDexBuff()), 120, 192); //Generate dexterity stat
         setImage(image);
         begin = true;
     }
