52fdca4e0d19af944aae5e6fd9f179280bc4b819
jackyyang09 <jackyyang267@gmail.com>
2015-06-04 23:50:07 -0400

    Update ProfileWindow.java

--- ProfileWindow.java
+++ ProfileWindow.java
@@ -5,11 +5,11 @@ import java.util.List;
  * Displays player information
  * 
  * @author Jacky Yang 
- * @version (a version number or a date)
+ * @version Beta Release
  */
 public class ProfileWindow extends Actor
 {
-    private boolean begin = false;
+    private boolean update = false;
     Font fontName, fontNums;
     public ProfileWindow()
     {
@@ -23,28 +23,25 @@ public class ProfileWindow extends Actor
      */
     public void act() 
     {
-        if (begin == false){begin();}
+        if (update == false){update();}
     }    
     
-    public void begin()
-    {
-        update();
-        begin = true;
-    }
-    
+    /**
+     * Generates the player's information and loads them onto the image
+     */
     public void update()
     {
-        GreenfootImage image = new GreenfootImage("profile.png");
+        GreenfootImage image = new GreenfootImage("profile.png"); //Prepare the image to contain all information
         //image.setFont(fontName);
         //Code to insert playername here
         image.setFont(fontNums);
         List<Player> Playerlist = getWorld().getObjects(Player.class);
         for(Player P : Playerlist)
         {
-            image.drawString(Integer.toString((int)(P.getAtk())), 70, 100);
-            image.drawString(Integer.toString((int)(P.getDef())), 195, 100);
-            image.drawString(Integer.toString((int)(P.getDex())), 70, 178);
-            image.drawString(Integer.toString((int)(P.getLuk())), 195, 178);
+            image.drawString(Integer.toString((int)(P.getAtk())), 70, 100); //Attack stat
+            image.drawString(Integer.toString((int)(P.getDef())), 195, 100); //Defense stat
+            image.drawString(Integer.toString((int)(P.getDex())), 70, 178); //Dexterity stat
+            image.drawString(Integer.toString((int)(P.getLuk())), 195, 178); //Luck stat
         }
         setImage(image);
     }
