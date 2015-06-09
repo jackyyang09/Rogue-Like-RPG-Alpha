4b4a9342eaf9884919501d53cfd316562bfedcfa
krisleung98 <krisleung98@gmail.com>
2015-06-09 11:08:59 -0400

    Update ScrollingMap.java

--- ScrollingMap.java
+++ ScrollingMap.java
@@ -184,7 +184,7 @@ public class ScrollingMap extends World
             for(int y = 0 ; y < MAPIMGHEIGHT;y++)
             {
                 for(int d = 0; d < MAPDEPTH; d++){
-                    if(data[x][y] != null){
+                    if(data[x][y][0] != null){
                         if(data[x][y][0].equals("door")){
                             field[x][y][0] = new Tile(x * TILESIZE + TILESIZE/2, y * TILESIZE + TILESIZE/2, 1);
                         }
