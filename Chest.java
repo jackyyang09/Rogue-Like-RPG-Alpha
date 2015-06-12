44d3e3daec8ebb8dd86206b1637b0fda54615527
Cypheruim <ryanhuang1234@hotmail.com>
2015-06-12 00:02:41 -0400

    Update Chest.java

--- Chest.java
+++ Chest.java
@@ -40,7 +40,7 @@ public class Chest extends Actor
         {
             int valX = (mapX - 43) / 86;
             int valY = (mapY - 43) / 86;
-            ((ScrollingMap)getWorld()).inputItem(mapX, mapY, generate(1)); //Adds item to an array in Scrolling Map
+            ((ScrollingMap)getWorld()).inputItem(valX, valY, generate(1)); //Adds item to an array in Scrolling Map
             ((ScrollingMap)getWorld()).removeMe(valX, valY, 5);
             getWorld().removeObject(this);
         }
