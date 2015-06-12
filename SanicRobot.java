43950b2a30bf06cb16dd02a06ce027ada0ce9f69
sean21x <sean21x@users.noreply.github.com>
2015-06-12 11:30:16 -0400

    Update SanicRobot.java

--- SanicRobot.java
+++ SanicRobot.java
@@ -10,6 +10,33 @@ import java.util.Random;
 public class SanicRobot extends Enemy
 {
     Random r;
+    public SanicRobot(int getMapX, int getMapY, int IDNum){
+        if (IDNum ==1)
+        {
+            setImage("sanicsrobot.png");
+            level = 1;
+            value = 39;
+            xp = 0;
+            baseHp = 10;
+            currentHp = baseHp;
+            baseAtt = 3;
+            attack = baseAtt;
+            baseDef = 5;
+            defense = baseDef;
+            baseDex = 0;
+            dexterity = baseDex;
+            baseHit = 3;
+            baseLuk = 5;
+            luck = baseLuk;
+            baseMove = 2;
+            move = 0;
+            levelGenerate(Greenfoot.getRandomNumber(5) + 1);
+        }
+        //grid2 = ((ScrollingMap)getWorld()).getGrid();
+        mapX = getMapX;
+        mapY = getMapY;
+    }
+    
     public void dropStuff(){
         r = new Random();
         
