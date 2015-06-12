318affcb718f93c4652f4c02a7b4e81394556257
sean21x <sean21x@users.noreply.github.com>
2015-06-12 11:29:57 -0400

    Update HolyDriver.java

--- HolyDriver.java
+++ HolyDriver.java
@@ -10,6 +10,34 @@ import java.util.Random;
 public class HolyDriver extends Enemy
 {
      Random r;
+     
+     public HolyDriver(int getMapX, int getMapY, int IDnum){
+        if (IDnum ==1)
+        {
+            setImage("beefbot.png");
+            level = 1;
+            value = 39;
+            xp = 0;
+            baseHp = 10;
+            currentHp = baseHp;
+            baseAtt = 3;
+            attack = baseAtt;
+            baseDef = 5;
+            defense = baseDef;
+            baseDex = 0;
+            dexterity = baseDex;
+            baseHit = 3;
+            baseLuk = 5;
+            luck = baseLuk;
+            baseMove = 2;
+            move = 0;
+            levelGenerate(Greenfoot.getRandomNumber(5) + 1);
+        }
+        //grid2 = ((ScrollingMap)getWorld()).getGrid();
+        mapX = getMapX;
+        mapY = getMapY;
+    }
+    
     public void dropStuff(){
         r = new Random();
         int random = r.nextInt(100);
