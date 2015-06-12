aa961ab569468945de5cd8f6369c9807ef8b3728
jackyyang09 <jackyyang267@gmail.com>
2015-06-12 01:56:55 -0400

    Create ExperienceBar.java

---
+++ ExperienceBar.java
@@ -0,0 +1,24 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class ExperienceBar here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ExperienceBar extends Actor
+{
+    public ExperienceBar()
+    {
+        setImage("exp.png");
+    }
+    
+    /**
+     * Act - do whatever the ExperienceBar wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
