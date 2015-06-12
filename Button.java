66dae59faf5c070ba9760b3bf6fab24642a7b09d
AingharanU <aingharan.u@outlook.com>
2015-06-12 02:50:44 -0400

    Update Button.java

--- Button.java
+++ Button.java
@@ -9,4 +9,28 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Button extends Actor
 {
+  /**
+     * Construct a Button with a given String at the default size
+     * 
+     * @param text  String value to display
+     * 
+     */
+    public Button ()
+    {
+        
+    }
+    
+  public Button (String text, int textSize)
+    {
+        buttonText = text;
+        GreenfootImage tempTextImage = new GreenfootImage (text, textSize, Color.RED, Color.WHITE);
+        myImage = new GreenfootImage (tempTextImage.getWidth() + 8, tempTextImage.getHeight() + 8);
+        myImage.setColor (Color.WHITE);
+        myImage.fill();
+        myImage.drawImage (tempTextImage, 4, 4);
+
+        myImage.setColor (Color.BLACK);
+        myImage.drawRect (0,0,tempTextImage.getWidth() + 7, tempTextImage.getHeight() + 7);
+        setImage(myImage);
+    }
 }
