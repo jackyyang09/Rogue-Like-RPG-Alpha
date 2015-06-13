d930522bea039ef39a4b09a00e524bea48735956
Brown Ninja <kajamugesh@hotmail.com>
2015-06-13 01:01:23 -0400

    Update SanicRobot.java

--- SanicRobot.java
+++ SanicRobot.java
@@ -5,7 +5,7 @@ import java.util.Random;
  * SanicRobot enemy
  * 
  * @author (Kajamugesh Raneethran) 
- * @version (a version number or a date)
+ * @version (June 12 2015)
  */
 public class SanicRobot extends Enemy
 {
