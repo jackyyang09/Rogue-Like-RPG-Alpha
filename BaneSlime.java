80664b5cb74587ab70ace4a5e84dcc79dde56a2d
Brown Ninja <kajamugesh@hotmail.com>
2015-06-13 00:40:55 -0400

    Update BaneSlime.java

--- BaneSlime.java
+++ BaneSlime.java
@@ -2,7 +2,7 @@ import greenfoot.*;
 import java.util.Random;
 
 /**
- * Write a description of class BaneSlime here.
+ * BaneSlime enemy
  * 
  * @author (Kajamugesh Raneethran) 
  * @version (a version number or a date)
