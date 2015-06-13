780005ca1154ec9aba4638103ff8f209e30d5d2f
Brown Ninja <kajamugesh@hotmail.com>
2015-06-12 23:39:41 -0400

    Update BaneSlime.java

--- BaneSlime.java
+++ BaneSlime.java
@@ -4,7 +4,7 @@ import java.util.Random;
 /**
  * Write a description of class BaneSlime here.
  * 
- * @author (your name) 
+ * @author (Kajamugesh Raneethran) 
  * @version (a version number or a date)
  */
 public class BaneSlime extends Enemy
